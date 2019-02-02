5c55b6ca27cb7f3f69980fb41def6ff89528d181
Derek Nguyen <derek@penandpillow.com>
2019-02-03 00:41:10 +0900

    first cut

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const { selectAll } = require('unist-util-select');
+
+// some shorthands
+const headingsToQuery = () => {
+  const result = {};
+  for (let i = 1; i < 7; i++) {
+    result[`h${i}`] = `heading[depth=${i}]`
+  };
+  return result;
+}
+
+const elToQuery = {
+  ...headingsToQuery(),
+  'p': 'paragraph',
+  'a': 'link',
+  'em': 'emphasis',
+  's': 'delete',
+}
+
+module.exports = ({ markdownAST: mdast }, pluginOptions) => {
+  const { plugins, ...propOfEl } = pluginOptions;
+
+  Object.keys(propOfEl).forEach(el => {
+    // apply shorthand
+    const query = elToQuery[el] || el;
+    const targetNodes = selectAll(query, mdast);
+
+    targetNodes.forEach(node => {
+      if (!node.data) node.data = {};
+
+      const props = propOfEl[el];
+      if (typeof props === "string" || Array.isArray(props)) {
+        node.data.hProperties = {
+          className: props,
+        }
+      }
+      else {
+        node.data.hProperties = props;
+      }
+    })
+  })
+}
